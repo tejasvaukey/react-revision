913b9b9296ed77bca2c44657e5ed15c41bdeaaff
Tejasva Ukey <tejasvaukey123@gmail.com>
2026-03-21 19:32:43 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+      <div id='parent'>
+        <h1 id='child1'>Hello</h1>
+        <h2 id='child2'>dost</h2>
+      </div>
+      <div id='parent2'></div>
+    </>
+  )
+}
+
+export default App
